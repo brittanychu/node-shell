1de9fcdff90e35983566348ef22df888d7635cd3
manja <anjiemerchant@gmail.com>
2018-01-15 12:31:31 -0500

    ctd

--- bash.js
+++ bash.js
@@ -4,14 +4,29 @@ let commands = require('./commands');
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', function(data) {
-  
-  var cmd = data.toString().trim();
+  // take data extract a space delimited arg string to pass into each of the fxns defined below cd ..
+  var cmd = data.toString().trim().split(" ")[0]
+  var remainingArgs = data.toString().trim().split(" ").slice(1).join(" ");
   if (cmd === "pwd") {
     commands.pwd();
   } else if (cmd === "date") {
     commands.date();
   } else if (cmd === 'ls'){
     commands.ls('.');
+  } else if (cmd === 'echo') {
+    commands.echo(remainingArgs);
+  } else if (cmd === "cat") {
+    commands.cat(remainingArgs);
+  } else if (cmd === "head") {
+    commands.head(remainingArgs);
+  } else if (cmd === "tail") {
+    commands.tail(remainingArgs);
+  } else if (cmd === "sort") {
+    commands.sort(remainingArgs);
+  } else if (cmd === "wc") {
+    commands.wc(remainingArgs);
+  } else if (cmd === "uniq") {
+    commands.uniq(remainingArgs);
   }
 
 
@@ -21,3 +36,5 @@ process.stdin.on('data', function(data) {
 // running head on this file (head bash.js) prints the first 12 lines of code
 // running a pipeline on this file (cat bash.js | head), runs both, but only returns the rightmost command
 
+//test
+//test
--- commands.js
+++ commands.js
@@ -2,23 +2,99 @@ var fs = require('fs');
 
 module.exports = {
 
-    date: function() {
+    date: function(filename) {
         process.stdout.write(new Date().toLocaleString());
         process.stdout.write('\nprompt >');
     },
 
-    pwd: function(){
+    pwd: function(filename){
         process.stdout.write(process.cwd());
         process.stdout.write('\nprompt >');
     },
 
-    ls: function(path){
-        return fs.readdir(path, function(err, files) {
+    ls: function(filename){
+        return fs.readdir(filename, function(err, files) {
             if (err) throw err;
             files.forEach(function(file) {
               process.stdout.write(file.toString() + "\n");
             })
-            process.stdout.write("prompt > ");
+            process.stdout.write("\nprompt > ");
+          });
+    },
+
+    echo: function(filename) {
+        process.stdout.write(filename);
+        process.stdout.write('\nprompt >');
+    },
+
+    cat: function(filename) {
+        return fs.readFile('./' + filename, 'utf8', function(err, data) {
+            if (err) throw err;
+            let content = data
+            process.stdout.write(content);
+            process.stdout.write("\nprompt > ");
+          });
+    },
+
+    head: function(filename) {
+        return fs.readFile('./' + filename, 'utf8', function(err, data) {
+            if (err) throw err;
+            let content = data
+            let lines = content.split('\n');
+            for (let i = 0; i < 5; i++) {
+                 process.stdout.write(lines[i] + '\n');
+            }
+
+            process.stdout.write("\nprompt > ");
+          });
+    },
+
+    tail: function(filename) {
+        return fs.readFile('./' + filename, 'utf8', function(err, data) {
+            if (err) throw err;
+            let content = data
+            let lines = content.split('\n');
+            for (let i = lines.length - 5; i < lines.length; i++) {
+                 process.stdout.write(lines[i] + '\n');
+            }
+
+            process.stdout.write("\nprompt > ");
+          });
+    },
+
+    sort: function(filename) {
+        return fs.readFile('./' + filename, 'utf8', function(err, data) {
+            if (err) throw err;
+            let content = data
+            let lines = content.split('\n');
+            process.stdout.write(lines.sort().join('\n'));
+            process.stdout.write("\nprompt > ");
+          });
+    },
+
+    wc: function(filename) {
+        return fs.readFile('./' + filename, 'utf8', function(err, data) {
+            if (err) throw err;
+            let content = data
+            let linesLength = content.split('\n').length;
+            process.stdout.write(linesLength.toString() + "\n");
+            process.stdout.write("\nprompt > ");
+          });
+    },
+
+    uniq: function(filename) {
+        return fs.readFile('./' + filename, 'utf8', function(err, data) {
+            if (err) throw err;
+            let content = data
+            let lines = content.split('\n');
+            let uniqArr = [];
+            lines.forEach(function(x) {
+                if (uniqArr.indexOf(x) === -1) {
+                    uniqArr.push(x);
+                }
+            })
+            process.stdout.write(uniqArr.join("\n"));
+            process.stdout.write("\nprompt > ");
           });
     }
 }
